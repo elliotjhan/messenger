d0f0a19834d81eb5d807697234e7e56c2b0a550b
elliotjhan <elliotjhan@gmail.com>
2022-06-16 11:18:51 -0700

    flex display with material ui button and search icon

--- messenger/components/Sidebar.tsx
+++ messenger/components/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { Avatar, Icon, IconButton } from '@material-ui/core';
+import { Avatar, Button, Icon, IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import SearchIcon from '@material-ui/icons/Search';
 
 const Sidebar = () => {
   return (
@@ -18,6 +18,12 @@ const Sidebar = () => {
           </IconButton>
         </IconsContainer>
       </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder='Search'/>
+      </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
     </Container>
   )
 }
@@ -27,11 +33,40 @@ export default Sidebar;
 const Container = styled.div`
 
 `;
-const Header = styled.div`
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+const SidebarButton = styled(Button)`
+  width: 100%;
 
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
 `;
 const UserAvatar = styled(Avatar)`
-  margin: 10px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
 `;
 const IconsContainer = styled.div`
   

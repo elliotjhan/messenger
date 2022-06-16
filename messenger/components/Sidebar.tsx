f370e7928415e347f032a2fc90d743167482b066
elliotjhan <elliotjhan@gmail.com>
2022-06-16 09:36:18 -0700

    styled components and material ui installed

---
+++ messenger/components/Sidebar.tsx
@@ -0,0 +1,38 @@
+import { Avatar, Icon, IconButton } from '@material-ui/core';
+import styled from 'styled-components';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const Sidebar = () => {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+    </Container>
+  )
+}
+
+export default Sidebar;
+
+const Container = styled.div`
+
+`;
+const Header = styled.div`
+
+`;
+const UserAvatar = styled(Avatar)`
+  margin: 10px;
+`;
+const IconsContainer = styled.div`
+  
+`;
